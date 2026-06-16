ed3070bed7aad94ec2236b23387277d7965063ea
Mihai3636 <mihai3636@email.com>
2026-06-16 21:30:41 +0300

    add searchMovies service method

--- src/app/core/movie-service.ts
+++ src/app/core/movie-service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { delay } from 'rxjs';
 import env from '../../environments/environment';
 import { TmdbResponse } from './movie.model';
 
@@ -8,7 +7,7 @@ import { TmdbResponse } from './movie.model';
   providedIn: 'root',
 })
 export class MovieService {
-  private readonly url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US';
+  private readonly url = 'https://api.themoviedb.org/3';
   private headers = {
     accept: 'application/json',
     Authorization: `Bearer ${env.tmdbReadToken}`,
@@ -17,11 +16,18 @@ export class MovieService {
 
   getTopRatedMovies(page: number) {
     console.log(`getTopRatedMovies(${page})`);
-    let urlWithPageParam = `${this.url}&page=${page}`;
+    let urlWithPageParam = `${this.url}/movie/top_rated?language=en-US&page=${page}`;
 
-    return this.httpClient.get<TmdbResponse>(urlWithPageParam, { headers: this.headers }).pipe(
-      delay(200),
+    return this.httpClient
+      .get<TmdbResponse>(urlWithPageParam, { headers: this.headers })
+      .pipe
+      // delay(200),
       // switchMap(() => throwError(() => new Error('Simulated error'))),
-    );
+      ();
+  }
+
+  searchMovies(query: string, page: number) {
+    const searchUrl = `${this.url}/search/movie?language=en-US&query=${encodeURIComponent(query)}&page=${page}`;
+    return this.httpClient.get<TmdbResponse>(this.url, { headers: this.headers });
   }
 }
